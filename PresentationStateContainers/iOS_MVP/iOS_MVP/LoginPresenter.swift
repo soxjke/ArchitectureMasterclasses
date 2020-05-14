c59e2d66e5fec315504e35ffbe7208a1c0d71291
Petro Korienev <soxjke@gmail.com>
2020-05-14 20:18:24 +0300

    MVP Step2: Added validation

--- PresentationStateContainers/iOS_MVP/iOS_MVP/LoginPresenter.swift
+++ PresentationStateContainers/iOS_MVP/iOS_MVP/LoginPresenter.swift
@@ -13,7 +13,9 @@ class LoginPresenter {
     weak var controller: LoginViewController?
     
     private var login: String?
+    private var loginError: String? = "Login should be 3 chars or more"
     private var password: String?
+    private var passwordError: String? = "Password should be 6 chars or more"
     
     init(controller: LoginViewController) {
         self.controller = controller
@@ -27,20 +29,25 @@ class LoginPresenter {
     private func makeProps() -> Props {
         return Props(
             login: login ?? "",
+            loginError: loginError,
             password: password ?? "",
+            passwordError: passwordError,
             loginEditingChanged: self.loginChanged,
             passwordEditingChanged: self.passwordChanged,
+            loginEnabled: loginError == nil && passwordError == nil,
             loginAction: self.loginAction
         )
     }
     
     private func loginChanged(_ string: String?) {
         login = string
+        loginError = (string?.count ?? 0 < 3) ? "Login should be 3 chars or more" : nil
         present()
     }
     
     private func passwordChanged(_ string: String?) {
         password = string
+        passwordError = (string?.count ?? 0 < 6) ? "Password should be 6 chars or more" : nil
         present()
     }
     
--- PresentationStateContainers/iOS_MVP/iOS_MVP/LoginViewController.swift
+++ PresentationStateContainers/iOS_MVP/iOS_MVP/LoginViewController.swift
@@ -11,22 +11,31 @@ import UIKit
 class LoginViewController: UIViewController {
     struct Props: Defaultable {
         let login: String
+        let loginError: String?
         let password: String
+        let passwordError: String?
         let loginEditingChanged: ValueCallback<String?>
         let passwordEditingChanged: ValueCallback<String?>
+        let loginEnabled: Bool
         let loginAction: Callback
         
         static var defaultValue = LoginViewController.Props(login: "",
+                                                            loginError: nil,
                                                             password: "",
+                                                            passwordError: nil,
                                                             loginEditingChanged: { _ in },
                                                             passwordEditingChanged: { _ in },
+                                                            loginEnabled: false,
                                                             loginAction: {})
     }
     private var props: Props = Props.defaultValue
     var retainedObject: Any?
     
     @IBOutlet private weak var loginField: UITextField!
+    @IBOutlet private weak var loginErrorLabel: UILabel!
     @IBOutlet private weak var passwordField: UITextField!
+    @IBOutlet private weak var passwordErrorLabel: UILabel!
+    @IBOutlet private weak var loginButton: UIButton!
     @IBAction func loginButtonDidPress(_ sender: UIButton) { props.loginAction() }
     @IBAction func loginEditingChanged(_ sender: UITextField) { props.loginEditingChanged(loginField.text) }
     @IBAction func passwordEditingChanged(_ sender: UITextField) { props.passwordEditingChanged(passwordField.text)  }
@@ -44,13 +53,22 @@ class LoginViewController: UIViewController {
     private func render() {
         renderLoginField()
         renderPasswordField()
+        renderLoginButton()
     }
     
     private func renderLoginField() {
         self.loginField.text = props.login
+        self.loginErrorLabel.text = props.loginError
+        self.loginErrorLabel.isHidden = props.loginError == nil
     }
     
     private func renderPasswordField() {
         self.passwordField.text = props.password
+        self.passwordErrorLabel.text = props.passwordError
+        self.passwordErrorLabel.isHidden = props.passwordError == nil
+    }
+    
+    private func renderLoginButton() {
+        self.loginButton.isEnabled = props.loginEnabled
     }
 }
